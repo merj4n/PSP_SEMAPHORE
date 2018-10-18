4ea3bde1c8dcf70b388a2f3a83b11fb326fcac93
merjan <merjan@gmail.com>
2018-10-18 20:50:50 +0200

    Update OK!

--- src/Buffer.java
+++ src/Buffer.java
@@ -3,15 +3,49 @@ import java.util.concurrent.Semaphore;
 
 public class Buffer {
 
-    int num;
-    Semaphore estado;
-    ArrayList<Integer> n = new ArrayList<>();
+    int maxTamanyo;
+    public Semaphore estado = new Semaphore(1);
 
-    Buffer (int num){
-        this.num=num;
+    ArrayList<String> lista = new ArrayList<>();
+
+    Buffer(int maxTamanyo) {
+        this.maxTamanyo = maxTamanyo;
     }
 
+    public synchronized void Almacenar (int id){ //SIN ESTA PUTA MIERDA NADA VALE EN LA VIDA
+        try {
+            if (maxTamanyo == this.lista.size()) {
+                System.out.println("Productor [" + id + "] esta esperando porque esta llena.");
+                return;
+            } else {
+                estado.acquire();
+                System.out.println("Productor [" + id + "] esta depositando.");
+                Thread.sleep(1000);
+                lista.add("Peras");
+                System.out.println("Productor [" + id + "] ha terminado.");
+                estado.release();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public synchronized void Consumir (int id){ // GG WELL PLAYED MIGUEL!!!
+        try {
+            if (lista.isEmpty()){
+                System.out.println("Consumidor [" + id + "] esta esperando porque esta vacia.");
+                return;
+            }else {
+                estado.acquire();
+                System.out.println("Consumidor [" + id + "] esta consumiendo.");
+                Thread.sleep(500);
+                lista.remove("Peras");
+                System.out.println("Consumidor [" + id + "] ha terminado.");
+                estado.release();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-
-
+    }
 }
+
--- src/Mercadillo.java
+++ src/Mercadillo.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mercadillo{
 
     public static void main(String[] args) {
 
-
-
-
+    Buffer bu = new Buffer(3);
+        List<Productor> lispr = new ArrayList<>();
+        List<Consumidor> liscr = new ArrayList<>();
+    for(int i=0;i<10;i++) {
+        lispr.add( new Productor(i, bu));
+        liscr.add(new Consumidor(i, bu));
+    }
+        for (Productor p:lispr) {
+            p.start();
+        }
+        for (Consumidor c:liscr) {
+            c.start();
+        }
     }
 }
